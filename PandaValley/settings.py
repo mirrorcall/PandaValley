73d3c10b6b1facc7865904365c968b6d129f7cf6
chrisly <chrizly07@gmail.com>
2020-03-20 22:40:41 +1100

    fix gmail security settings

--- PandaValley/settings.py
+++ PandaValley/settings.py
@@ -154,8 +154,6 @@ STATICFILES_DIRS = [
 
 # Email setting
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-EMAIL_USE_SSL = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
