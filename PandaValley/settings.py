fff48934003f92159c16991f6786b59442d1e48d
95cdy <a651728840@163.com>
2020-04-08 00:02:23 +1000

    fix bugs add new functions

--- PandaValley/settings.py
+++ PandaValley/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
 
     'apps.user',
     'apps.property',
-    'apps.search',
 ]
 
 MIDDLEWARE = [
--- apps/user/urls.py
+++ apps/user/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     re_path(r'join', views.JoinView.as_view()),
     re_path(r'login', views.LoginView.as_view()),
     re_path(r'forget_password', views.ForgetPassword.as_view()),
-    re_path(r'email_validation', views.EmailValidation.as_view()),
+    re_path(r'emailvalidation', views.EmailValidation.as_view()),
     re_path(r'reset_password', views.ResetPassword.as_view()),
     re_path(r'upload_avatar', views.UploadAvatarView.as_view()),
     re_path(r'pass_user_info', views.PassUserInfoView.as_view()),
