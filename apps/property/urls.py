d9e3bf026662b1a5471f5ea200ebe4195fe73db3
95cdy <a651728840@163.com>
2020-04-09 07:07:31 +1000

    change requirement for addproperty and my property functions

--- apps/property/urls.py
+++ apps/property/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     re_path(r'add_property', views.AddProperty.as_view()),
     re_path(r'search_property', views.SearchPropertyView.as_view()),
-    re_path(r'varify_booking', views.VerifyReserveView.as_view()),
+    ###new changes
+    re_path(r'edit_booking', views.VerifyReserveView.as_view()),
     re_path(r'show_property', views.ShowPropertyView.as_view()),
     re_path(r'show_reviews', views.ShowReviewsView.as_view()),
     re_path(r'add_review', views.AddReviewView.as_view()),
